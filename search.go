f49faee0b8e4bc585f7cdcfc826d250410a4cd0d
Syu fu <syu.fu.dev@gmail.com>
2025-01-25 15:38:23 +0900

    feat: Improve performance

--- search.go
+++ search.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"strings"
 
 	"github.com/cli/go-gh/v2/pkg/api"
 	graphql "github.com/shurcooL/graphql"
@@ -34,18 +35,22 @@ func Search(searchWords []string) ([]SearchResult, error) {
 func fetchSearchResults(client *api.GraphQLClient, searchWords []string) ([]SearchResult, error) {
 	var results []SearchResult
 
+	// Create the search query string by joining the searchWords with commas or 'OR' (depending on needs).
+	joinedSearchWords := strings.Join(searchWords, " ")
+
+	variables := map[string]interface{}{
+		"search": graphql.String(joinedSearchWords),
+	}
+
+	var q query
+
+	err := client.Query("searchCount", &q, variables)
+	if err != nil {
+		return nil, fmt.Errorf("failed to fetch search counts: %w", err)
+	}
+
+	// Process each search word and map the results.
 	for _, searchWord := range searchWords {
-		variables := map[string]interface{}{
-			"search": graphql.String(searchWord),
-		}
-
-		var q query
-
-		err := client.Query("searchCount", &q, variables)
-		if err != nil {
-			return nil, fmt.Errorf("failed to search for %s: %w", searchWord, err)
-		}
-
 		results = append(results, SearchResult{
 			searchWord:  searchWord,
 			searchCount: q.Search.RepositoryCount,
